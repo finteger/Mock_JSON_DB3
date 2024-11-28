e38fb013a1d4f2eb078abaf3be6e8e129975fed1
finteger <nashassociatesinc@gmail.com>
2024-11-27 17:53:10 -0700

    Update

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const app = express();
 const userRoutes = require('./routes/users.js');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
+const rateLimit = require('express-rate-limit');
 
 //Setting up view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+//Setup middleware for rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //15 minutes
+  max: 10,
+  message: 'Too many requests.  Please try later.',
+ });
+ 
+app.use(limiter);
 
 //Using express.Router() to organize routes
 app.use(userRoutes);
